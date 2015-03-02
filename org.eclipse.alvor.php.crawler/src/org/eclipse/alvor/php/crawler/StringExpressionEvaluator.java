a4c91ac46c9fb39212e060bd50887b03c2388264
tammurmas <tammurmas@gmail.com>
2015-03-02 17:23:23 +0200

    Infix operators
    Created support for concatenating strings inside variable assignments.

--- org.eclipse.alvor.php.crawler/src/org/eclipse/alvor/php/crawler/StringExpressionEvaluator.java
+++ org.eclipse.alvor.php.crawler/src/org/eclipse/alvor/php/crawler/StringExpressionEvaluator.java
@@ -7,6 +7,7 @@ import org.eclipse.php.internal.core.ast.nodes.ASTNode;
 import org.eclipse.php.internal.core.ast.nodes.Assignment;
 import org.eclipse.php.internal.core.ast.nodes.Expression;
 import org.eclipse.php.internal.core.ast.nodes.FunctionInvocation;
+import org.eclipse.php.internal.core.ast.nodes.InfixExpression;
 import org.eclipse.php.internal.core.ast.nodes.Program;
 import org.eclipse.php.internal.core.ast.nodes.Scalar;
 import org.eclipse.php.internal.core.ast.nodes.Variable;
@@ -23,6 +24,12 @@ import com.googlecode.alvor.string.StringSequence;
 @SuppressWarnings("restriction")
 public class StringExpressionEvaluator {
 	
+	/**
+	 * Creates and returns a new HotspotDescriptor at the given position
+	 * @param node
+	 * @param pos
+	 * @return
+	 */
 	public HotspotDescriptor evaluate(ASTNode node, Position pos)
 	{
 		try
@@ -36,6 +43,13 @@ public class StringExpressionEvaluator {
 		}
 	}
 	
+	/**
+	 * Evaluates the provided ASTNode to extract IAbstractStrings
+	 * @param node
+	 * @param pos
+	 * @return
+	 * @throws UnsupportedStringOpEx - gets thrown if the node is not supported
+	 */
 	private IAbstractString eval(ASTNode node, Position pos) throws UnsupportedStringOpEx
 	{
 		if(node instanceof FunctionInvocation)
@@ -45,6 +59,14 @@ public class StringExpressionEvaluator {
 		else if(node instanceof Variable)
 		{
 			return evalVariable((Variable)node, pos);
+		}
+		else if(node instanceof InfixExpression)
+		{
+			return evalInfixExpression((InfixExpression)node, pos);
+		}
+		else if (node instanceof Scalar && ((Scalar)node).getScalarType() == Scalar.TYPE_STRING)
+		{
+			return new StringConstant(new Position(pos.getPath(), node.getStart(), node.getLength()), ((Scalar)node).getStringValue(), null);
 		}
 		else
 		{
@@ -115,21 +137,74 @@ public class StringExpressionEvaluator {
 			//not sure if we need to check the operators here or not???
 			if((operator != Assignment.OP_EQUAL) && (operator != Assignment.OP_CONCAT_EQUAL))
 			{
-				throw new UnsupportedStringOpEx("Unknown assignment operator: " + assign.getOperationString(), pos);
+				throw new UnsupportedStringOpEx("Unknown assignment operator: \"" + assign.getOperationString()+"\"", pos);
 			}
 				
 			Expression assignedValue = assign.getRightHandSide();
+			Position varPos = new Position(pos.getPath(), v.getStart(), v.getLength());
+			
 			if(assignedValue instanceof Scalar)
 			{
 				//not sure if it should be the position of the occurence or the final hotspot???
-				Position varPos = new Position(pos.getPath(), v.getStart(), v.getLength());
 				options.add(new StringConstant(varPos, ((Scalar)assignedValue).getStringValue(), null));
 			}
+			else if(assignedValue instanceof InfixExpression)
+			{
+				options.add(eval(assignedValue, varPos));
+			}
 			else
-				throw new UnsupportedStringOpEx("Only the assignment of strings to variable values allowed at the moment!", pos);
+				throw new UnsupportedStringOpEx("Only the assignment of strings and the results of infix operations allowed!", pos);
 		}
 		
 		return new StringSequence(pos, options);
+	}
+	
+	/**
+	 * Evaluates the given infix expression
+	 * @param expr
+	 * @return StringSequence from the parts of the InfixExpression
+	 */
+	private IAbstractString evalInfixExpression(InfixExpression expr, Position pos)
+	{
+		//right now we only allow the concatenation of strings, everything else gets discarded
+		int operator = expr.getOperator();
+		if(operator == InfixExpression.OP_CONCAT)
+		{
+			List<IAbstractString> ops = new ArrayList<IAbstractString>();
+			
+			Expression left = expr.getLeft();
+			Expression right = expr.getRight();
+			
+			if(left instanceof InfixExpression || left instanceof Scalar)
+			{
+				ops.add(eval(left, pos));
+			}
+			else
+			{
+				throw new UnsupportedStringOpEx(
+						"Unsupported expression inside infix expression!",
+						new Position(pos.getPath(), left.getStart(),
+								left.getEnd()));
+			}
+			
+			if(right instanceof InfixExpression || right instanceof Scalar)
+			{
+				ops.add(eval(right, pos));
+			}
+			else
+			{
+				throw new UnsupportedStringOpEx(
+						"Unsupported expression inside infix expression!",
+						new Position(pos.getPath(), right.getStart(),
+								right.getEnd()));
+			}
+			
+			return new StringSequence(pos, ops);
+		}
+		else
+		{
+			throw new UnsupportedStringOpEx("Unsupported infix operator \""+InfixExpression.getOperator(operator)+"\"", pos);
+		}
 	}
 	
 }
